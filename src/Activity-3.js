7761f2e42009c08dc9bc79b4f90943fb0d84fd93
santosh2697 <ramsantosh34@msitprogram.net>
2022-07-13 19:28:12 +0530

    activity 6 is done

--- src/Activity-3.js
+++ src/Activity-3.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
+import "./App.css"
+import up from "./up.jpg";
+import down from "./down.jpg";
 
 export default function Products () {
-    var [tbody, setTbody] = useState([]);   
-    var thead = ["Product title","Category","Price","Rating"].map((a,k1) => <th key={k1}>{a}</th>);
+    var [tbody, setTbody] = useState([]);  
+    var [ascendingTitle, setAscendingTitle] = useState(down); 
+    var [ascendingCat, setAscendingCat] = useState(down); 
+    
     var products = require("./products.json");
     var allProducts = products.map((product) => {
         var [title,category,price] = [product["title"],product["category"],product["price"]];
@@ -32,10 +37,55 @@ export default function Products () {
         setTbody(filC)     
     }
 
+    var print = () => {
+        tbody.map((a) => console.log(a[0]))
+    }
+
+    var sortTitle = () => {
+        if (ascendingTitle === down){
+            setTbody(tbody.sort(function(a,b){
+                return a[0].localeCompare(b[0]);
+            }))
+            setAscendingTitle(up)
+        }
+        else{
+            setTbody(tbody.reverse(function(a,b){
+                return a[0].localeCompare(b[0]);
+            }))
+            setAscendingTitle(down)
+        }
+    }
+
+    var sortCat = () => {
+        if (ascendingCat === down){
+            setTbody(tbody.sort(function(a,b){
+                return a[1].localeCompare(b[1]);
+            }))
+            setAscendingCat(up)
+        }
+        else{
+            setTbody(tbody.reverse(function(a,b){
+                return a[1].localeCompare(b[1]);
+            }))
+            setAscendingCat(down)
+        }
+    }
+
+    var thead = ["Product title","Category","Price","Rating"].map((a,k1) => {
+        if (a === "Product title"){
+            return <th key={k1}>{a} <button onClick={sortTitle}><img src={ascendingTitle} alt="Arrow keys"></img></button></th>
+        }
+        else if (a === "Category"){
+            return <th key={k1}>{a} <button onClick={sortCat}><img src={ascendingCat} alt="Arrow keys"></img></button></th>
+        }
+        else { return <th key={k1}>{a}</th>}
+    });
+
     return (
         <>
         <input onChange={filterTitle} placeholder="Enter Title"></input>
         <input onChange={filterCategory} placeholder="Enter Category"></input>
+        <button onClick={print} type="submit">Submit</button>
             <table>
                 <thead><tr>{thead}</tr></thead>
                 <tbody>
